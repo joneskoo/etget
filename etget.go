318b339d5ebec02a454913a88091727fc81f2c1a
Joonas Kuorilehto <joneskoo@derbian.fi>
2016-07-03 19:13:28 +0300

    Move etget.go to project root

---
+++ etget.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	// "bufio"
+	"fmt"
+	// "os"
+
+	"github.com/joneskoo/etget/fetcher"
+	"github.com/joneskoo/etget/secrets"
+	// "golang.org/x/crypto/ssh/terminal"
+)
+
+func main() {
+    // reader := bufio.NewReader(os.Stdin)
+    // fmt.Print("www.energiatili.fi username: ")
+    // username, err := reader.ReadString('\n')
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// fmt.Println("www.energiatili.fi password:")
+	// password, err := terminal.ReadPassword(0)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	f, err := fetcher.New()
+	if err != nil {
+		panic(err)
+	}
+	
+	fmt.Println("Logging in…")
+	// err = f.Login(username, string(password))
+	err = f.Login(secrets.Username(), secrets.Password())
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Login OK. Downloading consumption data…")
+	json, err := f.ConsumptionReport()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%v\n", string(json))
+}
