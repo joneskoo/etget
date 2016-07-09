28694454f162f916538ae90f624a2571f2ab9644
Joonas Kuorilehto <joneskoo@derbian.fi>
2016-07-09 19:00:17 +0300

    Store credentials and write to file

--- etget.go
+++ etget.go
@@ -1,41 +1,113 @@
 package main
 
 import (
-	"bufio"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
 
 	"github.com/joneskoo/etget/fetcher"
 	"golang.org/x/crypto/ssh/terminal"
 )
 
-func main() {
-	reader := bufio.NewReader(os.Stdin)
+const (
+	// CredentialsFile is the file name where credentials are stored
+	CredentialsFile = "./credentials.json"
+
+	// OutputFile is the name of the file where we write output
+	OutputFile = "./power.json"
+)
+
+type credentials struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func promptCredentials() (username, password string, err error) {
 	fmt.Print("www.energiatili.fi username: ")
-	username, err := reader.ReadString('\n')
-	if err != nil {
-		panic(err)
+	if _, err = fmt.Scanln(&username); err != nil {
+		return
 	}
 
-	fmt.Println("www.energiatili.fi password:")
-	password, err := terminal.ReadPassword(0)
+	fmt.Println(username, " password:")
+	passwordBytes, err := terminal.ReadPassword(0)
 	if err != nil {
-		panic(err)
+		return
+	}
+	password = string(passwordBytes)
+	return
+}
+
+func readCachedCredentials(filename string) (username, password string, err error) {
+	var (
+		c credentials
+		b []byte
+	)
+
+	if b, err = ioutil.ReadFile(filename); err != nil {
+		return
 	}
 
-	f, err := fetcher.New()
-	if err != nil {
-		panic(err)
+	if err = json.Unmarshal(b, &c); err != nil {
+		return
 	}
 
-	fmt.Println("Logging in…")
-	err = f.Login(username, string(password))
+	username = c.Username
+	password = c.Password
+	return
+}
+
+func writeCachedCredentials(filename, username, password string) (err error) {
+	var b []byte
+	c := credentials{username, password}
+	if b, err = json.Marshal(c); err != nil {
+		return
+	}
+	if err = ioutil.WriteFile(filename, b, 0600); err != nil {
+		return
+	}
+	return
+}
+
+func main() {
+	username, password, err := readCachedCredentials(CredentialsFile)
 	if err != nil {
-		panic(err)
+		log.Println("Could not find cached credentials in", CredentialsFile)
+		log.Println("Please enter credentials, I will remember them")
+		if username, password, err = promptCredentials(); err != nil {
+			log.Fatalln(err)
+		}
 	}
 
-	fmt.Println("Login OK. Downloading consumption data…")
-	if err := f.ConsumptionReport(os.Stdout); err != nil {
-		panic(err)
+	var f fetcher.Fetcher
+	if f, err = fetcher.New(); err != nil {
+		log.Fatalln(err)
 	}
+
+	log.Println("Logging in…")
+	if err = f.Login(username, string(password)); err != nil {
+		log.Println("Could not log in:", err)
+		username, password, err = promptCredentials()
+		if err = f.Login(username, string(password)); err != nil {
+			log.Fatalln(err)
+		}
+	}
+	log.Println("Login OK.")
+
+	log.Println("Storing credentials to", CredentialsFile)
+	if err = writeCachedCredentials(CredentialsFile, username, password); err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println("Downloading consumption data…")
+	var fp *os.File
+	if fp, err = os.Create(OutputFile); err != nil {
+		log.Fatalln(err)
+	}
+	if err := f.ConsumptionReport(fp); err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println("OK! Wrote output to", OutputFile)
 }
