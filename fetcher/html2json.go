3244b943c0826d4819a0e4069915ebbb1341f8d6
Joonas Kuorilehto <joneskoo@derbian.fi>
2016-07-24 13:36:41 +0300

    Simplify date conversion logic

--- fetcher/html2json.go
+++ fetcher/html2json.go
@@ -1,9 +1,7 @@
 package fetcher
 
 import (
-	"bytes"
 	"errors"
-	"fmt"
 	"strings"
 )
 
@@ -18,35 +16,10 @@ const (
 	endDate   = ")"
 )
 
-// dateConverter replaces "new Date(1234)" with "\"1234\""
-func dateConverter(input string) (output string, err error) {
-	pos := 0
-	var buffer bytes.Buffer
-	for {
-		// Look for new Date(
-		start := strings.Index(input[pos:], startDate)
-		if start == -1 {
-			// No more matches, pass through the rest
-			if _, err = buffer.WriteString(input[pos:]); err != nil {
-				return "", err
-			}
-			break
-		}
-		start += pos
-		// Look for closing )
-		end := strings.Index(input[start:], endDate)
-		if end == -1 {
-			// Invalid format; got start marker but no end
-			return "", ErrorDateFormat
-		}
-		end += start
-		// Write everything except start and stop markers
-		if fmt.Fprintf(&buffer, `%v%v`, input[pos:start], input[start+len(startDate):end]); err != nil {
-			return "", err
-		}
-		pos = end + 1
-	}
-	return buffer.String(), nil
+// dateConverter replaces "new Date(1234)" with "1234"
+func dateConverter(input string) (output string) {
+	r := strings.NewReplacer(startDate, "", endDate, "")
+	return r.Replace(input)
 }
 
 // html2json finds an embedded javascript object in HTML and converts timestamps to integers
@@ -56,7 +29,7 @@ func html2json(data []byte) (json string, err error) {
 	start := strings.Index(s, startData)
 	end := start + strings.Index(s[start:], endData)
 	modelData := s[start+len(startData) : end]
-	// Convert dates to JSON
-	modelData, err = dateConverter(modelData)
+	// Convert dates to JSON (ie. strip new `Date(` and `)`
+	modelData = dateConverter(modelData)
 	return modelData, nil
 }
--- fetcher/html2json_test.go
+++ fetcher/html2json_test.go
@@ -9,26 +9,18 @@ func TestDateConverterPositive(t *testing.T) {
 	cases := []struct {
 		in,
 		want string
-		error bool
 	}{
 		{in: "no date in string", want: "no date in string"},
 		{in: "no (date in string", want: "no (date in string"},
-		{in: "no )date in string", want: "no )date in string"},
+		{in: "no )date in string", want: "no date in string"},
 		{in: "new Date(123)", want: "123"},
 		{in: "asdf, new Date(123), new Date(124), foo", want: "asdf, 123, 124, foo"},
 		{in: "a adf new Date(123) foo", want: "a adf 123 foo"},
 		{in: "\"StartValue\":new Date(1325368800000),\"", want: "\"StartValue\":1325368800000,\""},
-		{in: "\"StartValue\":new Date(1325368800000,\"", error: true},
 	}
 
 	for _, c := range cases {
-		out, err := dateConverter(c.in)
-		if c.error && err == nil {
-			t.Errorf("dateConverter(%q); expected error, got err=%q", c.in, err)
-		}
-		if !c.error && err != nil {
-			t.Errorf("dateConverter(%q); expected err=nil, got err=%q", c.in, err)
-		}
+		out := dateConverter(c.in)
 		if out != c.want {
 			t.Errorf("dateConverter(%q); expected %q, got %q", c.in, c.want, out)
 		}
