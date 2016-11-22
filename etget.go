6c7d5540cbe12311e4836e1a0eb0e00b896b6e87
Joonas Kuorilehto <joneskoo@derbian.fi>
2016-11-22 05:59:41 +0200

    Move credentials to its own file

--- etget.go
+++ etget.go
@@ -1,70 +1,15 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
-	"fmt"
 	"io"
-	"io/ioutil"
 	"log"
 	"os"
 
 	"github.com/joneskoo/etget/fetcher"
-	"golang.org/x/crypto/ssh/terminal"
 )
 
 const jsonIndent = "    "
-
-type credentials struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
-}
-
-func promptCredentials() (username, password string, err error) {
-	fmt.Print("www.energiatili.fi username: ")
-	if _, err = fmt.Scanln(&username); err != nil {
-		return
-	}
-
-	fmt.Println(username, " password:")
-	passwordBytes, err := terminal.ReadPassword(0)
-	if err != nil {
-		return
-	}
-	password = string(passwordBytes)
-	return
-}
-
-func readCachedCredentials(filename string) (username, password string, err error) {
-	var (
-		c credentials
-		b []byte
-	)
-
-	if b, err = ioutil.ReadFile(filename); err != nil {
-		return
-	}
-
-	if err = json.Unmarshal(b, &c); err != nil {
-		return
-	}
-
-	username = c.Username
-	password = c.Password
-	return
-}
-
-func writeCachedCredentials(filename, username, password string) (err error) {
-	var b []byte
-	c := credentials{username, password}
-	if b, err = json.MarshalIndent(c, "", jsonIndent); err != nil {
-		return
-	}
-	if err = ioutil.WriteFile(filename, b, 0600); err != nil {
-		return
-	}
-	return
-}
 
 type ConsumptionFetcher interface {
 	Login(username, password string) error
